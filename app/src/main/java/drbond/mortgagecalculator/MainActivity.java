b9d4f31c49802608c21a989e2547f14a99ea25a1
Ryan Bond <drbond@gmail.com>
2017-07-01 11:08:44 -0500

    initial commit

---
+++ app/src/main/java/drbond/mortgagecalculator/MainActivity.java
@@ -0,0 +1,20 @@
+package drbond.mortgagecalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
